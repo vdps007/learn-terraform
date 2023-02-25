3e386ffd6d48026d18f50b579d87452e4feae70a
vdps <vdps007@gmail.com>
2023-02-25 20:06:59 +0530

    user file

--- variable/5var.tf
+++ variable/5var.tf
@@ -3,7 +3,7 @@ variable "sample" {
 }
 
 output "sample" {
-    value = "variable sample - ${var.sample}, variable sample1 - ${var.sample1}, Boolean Value of Map = ${var.sample2["boolean"]}"
+    value = "variable sample - ${var.sample}, variable sample1 - ${var.sample1}"
 }
 
 variable "sample1"{
