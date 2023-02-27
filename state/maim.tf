afa7b1aa1f7a6feff3c0bbe911a7348cc55cbe5a
vdps <vdps007@gmail.com>
2023-02-27 09:49:21 +0530

    user file

---
+++ state/maim.tf
@@ -0,0 +1,16 @@
+terraform {
+    backend "s3" {
+        bucket = "terraform-b71"
+        key = "satate/terraform.tfstate"
+        region = "us-east-1"
+    }
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-0a017"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0f"]
+  tags = {
+    name = "demo"
+  }
+}
