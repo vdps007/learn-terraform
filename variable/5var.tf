d2d8f408371ea7091e2d5f4886fa7d2e8d0e918c
vdps <vdps007@gmail.com>
2023-02-27 07:27:48 +0530

    user file

--- variable/5var.tf
+++ variable/5var.tf
@@ -3,12 +3,20 @@ variable "sample" {
 }
 
 output "sample" {
-    value = "variable sample - ${var.sample}, variable sample1 - ${var.sample1}, variable sample2 - ${var.sample2["number"]}"
+    value = "variable sample - ${var.sample}, variable sample3 -${var.sample3[3]} variable sample1 - ${var.sample1}, variable sample2 - ${var.sample2["number"]}"
 }
 
 variable "sample1"{
     default="hello sample1"
 }
+
+variable "sample3" {
+    default = {
+        "hello",
+        1000,
+        true,
+        "world"
+    }  
 
 variable "sample2" {
     default = {
