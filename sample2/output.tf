3851e1fa79d15f2e59c7db3e660012fa389db878
vdps <vdps007@gmail.com>
2023-03-02 14:34:19 +0530

    sample2 update

--- sample2/output.tf
+++ sample2/output.tf
@@ -1,6 +1,5 @@
-output "sample" {
-    value = [data.aws_caller_identity.current.account_id]
-}
-
 data "aws_caller_identity" "current" {}
 
+output "data" {
+    value = [data.aws_caller_identity.current.account_id]
+}
