62712545b27f509a2784d7b56f17e203fa1795ca
vdps <vdps007@gmail.com>
2023-03-03 14:58:31 +0530

    sample update

--- sample1/main.tf
+++ sample1/main.tf
@@ -1,16 +1,3 @@
-module "ec2" {
-  for_each      = var.instances
-  source        = "./test"
-  component     = each.value["name"]
-  instance_type = each.value["type"]
-  //password      = try(each.value["password"], null)
-}
-
-variable "instances" {
-  default = {
-    frontend = {
-      name = "test_frontend"
-      type = "t2.micro"
-    }
-  }
-}
+- name: Print frontend
+  debug:
+    msg: "frontend"
