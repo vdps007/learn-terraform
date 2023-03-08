eb6a059b10c43eab75a20661423e436f562f76f8
vdps <vdps007@gmail.com>
2023-03-08 10:53:35 +0530

    prometheus file adding

--- prometheus/main.tf
+++ prometheus/main.tf
@@ -9,6 +9,12 @@ resource "aws_spot_instance_request" "node1" {
   }
 }
 
+resource "aws_ec2_tag" "node1" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-server"
+}
+
 resource "aws_spot_instance_request" "node2" {
   ami           = "ami-0a017d8ceb274537d"
   instance_type = "t2.micro"
@@ -18,4 +24,10 @@ resource "aws_spot_instance_request" "node2" {
   tags = {
     Name = "prom-test-node"
   }
+}
+
+resource "aws_ec2_tag" "example" {
+  resource_id = aws_spot_instance_request.node2.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-node"
 }
