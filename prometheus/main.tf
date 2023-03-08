4899789fb84f7ea4710a1c9123ea8a5a7cc1da40
vdps <vdps007@gmail.com>
2023-03-08 10:46:04 +0530

    prometheus file adding

--- prometheus/main.tf
+++ prometheus/main.tf
@@ -9,7 +9,7 @@ resource "aws_spot_instance_request" "node1" {
   }
 }
 
-resource "aws_spot_instance_request" "node1" {
+resource "aws_spot_instance_request" "node2" {
   ami           = "ami-0a017d8ceb274537d"
   instance_type = "t2.micro"
   vpc_security_group_ids = [sg-08e7fba48c1aa9cc4]
