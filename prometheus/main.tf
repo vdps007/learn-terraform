6a57b223f797a4457c4196b6a5ab8cffa6c3d3a6
vdps <vdps007@gmail.com>
2023-03-11 16:41:57 +0530

    test update

--- prometheus/main.tf
+++ prometheus/main.tf
@@ -2,7 +2,9 @@ resource "aws_spot_instance_request" "node1" {
   ami           = "ami-0a017d8ceb274537d"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-08e7fba48c1aa9cc4"]
-  wait_for_fulfillment = true
+
+  // wait_for_fulfillment will be used for only spot instances
+  wait_for_fulfillment = true 
 
   tags = {
     Name = "prom-test-server"
